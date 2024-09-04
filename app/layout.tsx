419195af5651de9fa76abb87b513afd0a6da5333
FullStackSimon <fullstacksimon@gmail.com>
2024-09-04 17:35:38 +0200

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
 import { metaData } from "@/data/index";
 
 const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleAnalytics />
+      <SpeedInsights />
       <body className={`${lato.className} bg-primary-backgroundGreen`}>
         {children}
         <Analytics />
