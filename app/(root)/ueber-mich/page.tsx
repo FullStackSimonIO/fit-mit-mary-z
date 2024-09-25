d4f922f97ba886d64a5e46d6f3af675e153df38c
FullStackSimon <fullstacksimon@gmail.com>
2024-09-25 09:20:32 +0200

    fixed path

--- app/(root)/ueber-mich/page.tsx
+++ app/(root)/ueber-mich/page.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 import React from "react";
 import { Ausbildungen } from "@/components/experimental/Ausbildungen";
 import { Metadata } from "next";
-import AboutFirst from "@/components/AboutFirst";
+import AboutFirst from "@/components/experimental/AboutFirst";
 
 export const metadata: Metadata = {
   title: "Fit-Mit-Mary | Über mich",
